98262809b448f73be582414d2b283d4dbb4fb626
Erast Stinca <eric.stinca@gmail.com>
2019-07-19 16:04:53 +0300

    Added: header and main component, layout module, and routing to main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared/shared.module';
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
+
+    LayoutModule,
+
     BrowserModule,
     AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    MainComponent
+  ],
+  imports: [
+    SharedModule
+
+  ],
+  exports: [
+  ]
+})
+export class LayoutModule { }
